152942ee5e799e415bbdcf7aed045076f7fae852
ZwiniMic <zwirunio@gmail.com>
2020-04-12 22:17:52 +0200

    HomeWork add e and ulersSeries in Graphql server.

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 const { ApolloServer, gql } = require("apollo-server");
-
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 const typeDefs = gql`
-    type Query {
-        greeting: String
-        interestingUrls: [String]
-        firstName: String
-        email: String
-        pets: [String]
-    }
+  type Query {
+    greeting: String
+    interestingUrls: [String]
+    randomDiceThrow: Int
+    e: Float
+    eulersSeries: [Float]
+  }
 `;
 
-const data = {
-    greeting: "Hello World!",
-    interestingUrls: ["https://google.com", "https://kursreacta.pl", "https://graphql.org/"],
-    firstName: "John",
-    email: "john@example.com",
-    pets: ["Mittens", "Doggo", "Birb"]
-}
+const rootValue = () => {
+  const getRandomDiceThrow = (sides = 6) => Math.ceil(Math.random() * sides);
+  const getEutersSeries = (n = 1000) => {
+    var eulersSeries = [];
+    for (var i = 0; i < n; i++) {
+      eulersSeries.push(Math.pow(1 + 1 / (i + 1), i + 1));
+    }
 
-const server = new ApolloServer({ typeDefs, rootValue: data, introspection: true, playground: true });
+    return eulersSeries;
+  };
 
-server.listen({ port: PORT }).then((result) => console.log(result.url));
+  return {
+    greeting: "Hello world!",
+    interestingUrls: ["https://kursreacta.pl", "https://64bites.com"],
+    randomDiceThrow: getRandomDiceThrow(),
+    e: Math.E,
+    eulersSeries: getEutersSeries()
+  };
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  rootValue,
+  introspection: true,
+  playground: true
+});
+
+server.listen({ port: PORT }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
